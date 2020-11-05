f92f4cb71ac651eff3adbcb334daf39948588196
s-berntsson <susanna.berntsson2000@gmail.com>
2020-11-05 10:10:05 +0100

    shown word script

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ document.getElementById('start-button').addEventListener('click', function() {
 /*-----------------------THE-GAME----------------------------*/
 
 const words = ['coffee', 'programming', 'interface', 'whiskey', 'love', 'glowworm', 'jigsaw', 'bikini', 'buzzard', 'thumbscrew', 'transplant', 'python', 'unknown', 'index', 'xylophone', 'zombie', 'cassandra', 'sanna', 'wave', 'rhythm', 'subway', 'unworthy', 'witchcraft'];
+var word = Math.floor(Math.random() * words.length);
+
+var shownWord = document.getElementById("shown-word");
+
+for (var i=0; i<word.length; i++) {
+    shownWord.innerHTML += "<li>_</li>";
+}
