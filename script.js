5ab208def6df428e75b431ca7b6eb29f816cbbbf
Cenglesson <englesson@live.se>
2020-11-05 09:35:39 +0100

    added js code Start-frame

---
+++ script.js
@@ -0,0 +1,9 @@
+/*-----------------------START-FRAME-------------------------*/
+
+document.getElementById('start-button').addEventListener('click', function() {
+    //Toggle hide class
+    document.getElementById('chapter-one').classList.toggle('hide');
+    document.getElementById('start-frame').classList.toggle('hide');
+});
+
+/*-----------------------THE-GAME----------------------------*/
