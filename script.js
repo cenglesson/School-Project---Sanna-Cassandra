8901b9364fb0d294b12173491e59d1762f2fd279
Cenglesson <englesson@live.se>
2020-11-10 21:13:36 +0100

    notification container added

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ var theWordWas = document.getElementById("the-word-was");
 var pointCounter = document.getElementById("point-counter");
 var pointContainer = document.getElementById("point-container");
 var pointResult = document.getElementById("point-result");
+var notification = document.getElementById('notification-container');
 
 // Create variables for "future Use"
 var points = 0;
@@ -33,13 +34,13 @@ const words = ['coffee', 'programming', 'interface', 'whiskey', 'love', 'glowwor
 // Prepare word and lists for the game
 var setTheWord = () => {
     // Choose a random word from the array of words
- word = words[Math.floor(Math.random() * words.length)];
- console.log(word);
-// Create a list of lines with as many lines as there are letters in the chosen word
-for (var i=0; i<word.length; i++) {
-    shownWord.innerHTML += "<li>_</li>";
-    rightWord.innerHTML += "<li>" + word[i] + "</li>";
-};
+    word = words[Math.floor(Math.random() * words.length)];
+    console.log(word);
+    // Create a list of lines with as many lines as there are letters in the chosen word
+    for (var i = 0; i < word.length; i++) {
+        shownWord.innerHTML += "<li>_</li>";
+        rightWord.innerHTML += "<li>" + word[i] + "</li>";
+    };
 };
 
 // Invoke function
@@ -48,51 +49,61 @@ setTheWord();
 // Insert points in container
 pointContainer.innerHTML = points;
 
+// SHOW NOTIFICATION
+
+function showNotification() {
+    notification.classList.add('show');
+
+    setTimeout(() => {
+        notification.classList.remove('show');
+    }, 2000);
+}
+
 // Make a guess by pressing letter on keyboard
 window.addEventListener("keydown", e => {
     if (e.keyCode >= 65 && e.keyCode <= 90) {
         const letter = e.key;
 
-    // If the letter has already been guessed, console the log
-    if (shownWord.innerHTML.includes("<li>" + letter + "</li>") || wrongGuesses.innerHTML.includes("<li>" + letter + "</li>")) {
-        console.log("Already guessed!")
+        // If the letter has already been guessed, console the log
+        if (shownWord.innerHTML.includes("<li>" + letter + "</li>") || wrongGuesses.innerHTML.includes("<li>" + letter + "</li>")) {
+            showNotification();
 
-    // If the letter is in the word - show it in the right position
-    } else if (word.includes(letter)) {
-        for (var i=0; i<word.length; i++) {
-            if (letter === word[i]) {
-            shownWord.children[i].innerHTML = letter;
+            // If the letter is in the word - show it in the right position
+        } else if (word.includes(letter)) {
+            for (var i = 0; i < word.length; i++) {
+                if (letter === word[i]) {
+                    shownWord.children[i].innerHTML = letter;
+                }
             }
-        } 
-    
-        // If the word is completed show "win-PopUp" and add points
-        if (shownWord.innerHTML === rightWord.innerHTML) {
-            winPopup.classList.toggle('hide');
-            points += (6 - errorCounter);
-            pointContainer.innerHTML = points;      
-         }
 
-    // If the letter is not in the word, add it to the wrong-guesses-list
-    } else {
-        wrongGuesses.innerHTML += "<li>" + letter + "</li>";
-        
-        // Show each part of the figure for each error
-        figureParts.forEach((element, index) => {
-            if (index === errorCounter) {
-                element.classList.toggle("hide");
+            // If the word is completed show "win-PopUp" and add points
+            if (shownWord.innerHTML === rightWord.innerHTML) {
+                winPopup.classList.toggle('hide');
+                points += (5 - errorCounter);
+                pointContainer.innerHTML = points;
+            }
+
+            // If the letter is not in the word, add it to the wrong-guesses-list
+        } else {
+            wrongGuesses.innerHTML += "<li>" + letter + "</li>";
+
+            // Show each part of the figure for each error
+            figureParts.forEach((element, index) => {
+                if (index === errorCounter) {
+                    element.classList.toggle("hide");
+                };
+            });
+
+            // Add one more error to the counter
+            errorCounter += 1;
+
+            // When error counter reaches five show the "lose-PopUp"
+            if (errorCounter === 5) {
+                losePopup.classList.toggle("hide");
+                theWordWas.innerHTML = word;
+                pointResult.innerHTML = `You got ${points} points.`;
             };
-        });
- 
-        // Add one more error to the counter
-        errorCounter += 1;
-        
-        // When error counter reaches five show the "lose-PopUp"
-        if (errorCounter === 6 ){
-            losePopup.classList.toggle("hide");
-            theWordWas.innerHTML = word;
-            pointResult.innerHTML = `You got ${points} points.`;
-        };
-    }
+        }
     }
 });
 
@@ -103,10 +114,10 @@ var resetGame = () => {
     wrongGuesses.innerHTML = " ";
     errorCounter = 0;
     // Hide figure parts that are showing
-    figureParts.forEach( element => {
-        if (element.classList.contains("hide")=== false){
-        element.classList.toggle("hide")
-    };
+    figureParts.forEach(element => {
+        if (element.classList.contains("hide") === false) {
+            element.classList.toggle("hide")
+        };
     });
 
     // Randomize new word
@@ -116,11 +127,11 @@ var resetGame = () => {
 // Play again button - If you win
 playAgainBtn.addEventListener('click', () => {
     resetGame();
-    winPopup.classList.toggle("hide");   
+    winPopup.classList.toggle("hide");
 });
 
 // Play again button -  If you lose
-playAgainBtn2.addEventListener('click', () =>{
+playAgainBtn2.addEventListener('click', () => {
     resetGame();
     losePopup.classList.toggle("hide");
     points = 0;
